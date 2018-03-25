f9f1975080fedbc0d4f366dde84bbf8c88f721e7
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-25 19:18:12 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,40 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(user_input)
+  user_input = user_input.to_i
+  user_input = user_input - 1
+end
+input_to_index("1") # => 0
+input_to_index("6") # => 5
+input_to_index("invalid") # => -1
+
+  def move(board, index, character = "X")
+    # puts move[board, index, character]
+    # position = gets.strip
+    # character = X
+  board[index] = character
+  # move(board, 2)
+  # puts move(board)
+end
+
+#       move(board, 0, "X")
+#       move(board, 4, "O")
+#       move(board, 8)
+#       move(board, 0, "O")
+#       move(board, 8, "X")
+#       move(board, 0)
+#       move(board, 4)
+#       move(board, 8)
+
+  # def board = (9, " ")
+  #     move(board, 0, "X")
+  #     move(board, 1, "O")
+  #     move(board, 2, "X")
+  #     move(board, 3, "O")
+  #     move(board, 4, "X")
+  #     move(board, 5, "O")
+  #     move(board, 6, "X")
+  #     move(board, 7, "X")
+  #     move(board, 8, "O")
+  # end
