7bd792555268737d47a9e038d51aace912e8514d
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-25 19:19:15 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,32 +14,32 @@ input_to_index("1") # => 0
 input_to_index("6") # => 5
 input_to_index("invalid") # => -1
 
-  def move(board, index, character = "X")
-    # puts move[board, index, character]
-    # position = gets.strip
-    # character = X
-  board[index] = character
-  # move(board, 2)
-  # puts move(board)
-end
+#   def move(board, index, character = "X")
+#     # puts move[board, index, character]
+#     # position = gets.strip
+#     # character = X
+#   board[index] = character
+#   # move(board, 2)
+#   # puts move(board)
+# end
 
-#       move(board, 0, "X")
-#       move(board, 4, "O")
-#       move(board, 8)
-#       move(board, 0, "O")
-#       move(board, 8, "X")
-#       move(board, 0)
-#       move(board, 4)
-#       move(board, 8)
+# #       move(board, 0, "X")
+# #       move(board, 4, "O")
+# #       move(board, 8)
+# #       move(board, 0, "O")
+# #       move(board, 8, "X")
+# #       move(board, 0)
+# #       move(board, 4)
+# #       move(board, 8)
 
-  # def board = (9, " ")
-  #     move(board, 0, "X")
-  #     move(board, 1, "O")
-  #     move(board, 2, "X")
-  #     move(board, 3, "O")
-  #     move(board, 4, "X")
-  #     move(board, 5, "O")
-  #     move(board, 6, "X")
-  #     move(board, 7, "X")
-  #     move(board, 8, "O")
-  # end
+#   # def board = (9, " ")
+#   #     move(board, 0, "X")
+#   #     move(board, 1, "O")
+#   #     move(board, 2, "X")
+#   #     move(board, 3, "O")
+#   #     move(board, 4, "X")
+#   #     move(board, 5, "O")
+#   #     move(board, 6, "X")
+#   #     move(board, 7, "X")
+#   #     move(board, 8, "O")
+#   # end
